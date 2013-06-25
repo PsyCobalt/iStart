269016835677295ae7c176461d80d727e9b4d8c9
Rich Brantingham <rich.brantingham@me.com>
2013-06-25 10:17:06 +0100

    mods to rb config as a git test.

--- src/iStarter/config/dev_rb.py
+++ src/iStarter/config/dev_rb.py
@@ -4,13 +4,13 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-     ('Rich Brantingham', 'brantinr@ptn.dstl.uk.vl'),
+     ('**************', '******************'),
 )
 
 
-EMAIL_HOST_USER     = 'brantinghamr@hotmail.com'
-EMAIL_HOST_PASSWORD = '*******'
-EMAIL_PORT          = 587
-EMAIL_HOST          = 'smtp.live.com'
-EMAIL_USE_TLS       = True
-FEEDBACK_RECIPIENTS = ['rich.brantingham@me.com',]
+#EMAIL_HOST_USER     = 'brantinghamr@hotmail.com'
+#EMAIL_HOST_PASSWORD = '*******'
+#EMAIL_PORT          = 587
+#EMAIL_HOST          = 'smtp.live.com'
+#EMAIL_USE_TLS       = True
+#FEEDBACK_RECIPIENTS = ['rich.brantingham@me.com',]
